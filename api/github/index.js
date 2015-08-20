44d841618ef04e8ff9652092da6820676853f8a1
Lance Hudson <lance@lancehudson.com>
2015-08-20 15:44:25 -0400

    stash

--- api/github/index.js
+++ api/github/index.js
@@ -4,6 +4,11 @@
 var debug = require('debug')('api:github');
 var scmp = require('scmp');
 var crypto = require('crypto');
+var tmp = require('tmp');
+var clone = require('nodegit').Clone.clone;
+var Q = require('q');
+var os = require('os');
+var rimraf = require('rimraf');
 
 // TODO get this from db based on url
 var key = '111';
@@ -27,14 +32,19 @@ var events = {
   push: function* pushEventHandler() {
     debug('push event');
 
+    // TODO: Check if configured ref
+    if (this.request.body.ref !== 'refs/heads/master') {
+      this.body = 'ref unconfigured, aborting.';
+      this.type = 'text/plain';
+      this.status = 202;
+      return;
+    }
+
     this.body = 'Build Pending';
     this.type = 'text/plain';
     this.status = 202;
 
-
-    // TODO: Check if configured ref
-
-    // Begin build pipeline
+    yield build.call(this.request.body);
   },
 };
 
@@ -106,6 +116,97 @@ var verifySignature = function() {
 
   return true;
 };
+
+// jscs:disable requireCamelCaseOrUpperCaseIdentifiers
+// jscs:disable requireCapitalizedComments
+
+var build = function* () {
+  // TODO: make api call to update status
+  // TODO: clone repo
+  debug('update repo status with clone pending',
+    this.repository.full_name, // jshint ignore:line
+    this.head_commit.id, // jshint ignore:line
+    'pending', 'Running clone'
+  );
+
+  this.tmpPath = yield createTmpDir();
+  yield cloneRepo.call(this);
+
+  // TODO: docker build
+  debug('update repo status with build pending',
+    this.repository.full_name, // jshint ignore:line
+    this.head_commit.id, // jshint ignore:line
+    'pending', 'Running build'
+  );
+
+  // TODO: docker tag
+
+  // TODO: docker push
+  debug('update repo status with build pending',
+    this.repository.full_name, // jshint ignore:line
+    this.head_commit.id, // jshint ignore:line
+    'pending', 'Pushing'
+  );
+
+  debug('update repo status with success',
+    this.repository.full_name, // jshint ignore:line
+    this.head_commit.id, // jshint ignore:line
+    'success', 'The build succeeded!'
+  );
+
+  // Cleanup
+  yield cleanupTmpDir.call(this);
+};
+
+var createTmpDir = function* () {
+  var defer = Q.defer();
+
+  tmp.dir({unsafeCleanup: true}, function(err, path) {
+    if (err) {
+      defer.reject(new Error(err));
+    }
+    debug('Temp Dir: ', path);
+    defer.resolve(path);
+  });
+
+  return defer.promise;
+};
+
+var cleanupTmpDir = function* () {
+  var defer = Q.defer();
+
+  rimraf(this.tmpPath, function(err) {
+    if (err) {
+      defer.reject(new Error(err));
+    }
+    defer.resolve();
+  });
+
+  return defer.promise;
+};
+
+var cloneRepo = function* () {
+  var ctx = this;
+  var cloneOptions = {};
+
+  // HTTPS certs don't work on OSX, great!
+  if (os.platform() === 'darwin') {
+    cloneOptions.remoteCallbacks = {
+      certificateCheck: function() { return 1; },
+    };
+  }
+
+  yield clone(ctx.repository.clone_url, // jshint ignore:line
+    ctx.tmpPath,
+    cloneOptions)
+    .then(function(repo) {
+      debug('Getting Commit: ', ctx.head_commit.id);  // jshint ignore:line
+      return repo.getCommit(ctx.head_commit.id);  // jshint ignore:line
+    });
+};
+
+// jscs:enable requireCamelCaseOrUpperCaseIdentifiers
+// jscs:enable requireCapitalizedComments
 
 // Expose exec
 exports.exec = exec;
